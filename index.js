284cc66e3eede42b2c10381d672112d766d49707
tylernappy <tylernappy@gmail.com>
2016-06-10 14:15:35 -0700

    fixed dotenv urls and removed debugger

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ var path = require('path')
 
 if (env == 'dev') {
   require('dotenv').load()
-  var twiMLUrl = "http://47c0032f.ngrok.io/twilioVoice"
-} else {
-  // var twiMLUrl = path.join(__dirname, 'twilioVoice')
-  var twiMLUrl = "https://callcenteranalytics.herokuapp.com/twilioVoice"
 }
 
-debugger
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
@@ -21,13 +16,6 @@ app.use(urlencoded)
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-// app.locals({
-//   makeTable  : function(obj) {
-//     var template = fs.readFileSync('makeList.ejs', 'utf-8')
-//     return ejs.render(template, list)
-//   }
-// })
 
 var port = process.env.PORT || 5000
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/test'
@@ -97,13 +85,10 @@ app.get('/', function(req, res) {
 })
 
 app.get('/about', function(req, res) {
-  res.render('about', {
-
-  })
+  res.render('about', {})
 })
 
 app.post('/search', function(req, res) {
-  debugger
   var searchText = req.body.search
   var data = {text: searchText, index: process.env.HOD_INDEX_NAME, print: 'all'}
   hodClient.call('querytextindex', data, function(err, resp, body) {
@@ -112,7 +97,6 @@ app.post('/search', function(req, res) {
         if (resp.body.status == 'failed') {
           var documents = []
         } else {
-          debugger
           var documents = resp.body.documents
         }
         res.render('search_results', {
@@ -124,13 +108,11 @@ app.post('/search', function(req, res) {
 })
 
 app.get('/findSimilar', function(req, res) {
-  debugger
   var indexReference = req.query.indexReference
   var data = {index_reference: indexReference, indexes: process.env.HOD_INDEX_NAME, print: 'all'}
   hodClient.call('findsimilar', data, function(err, resp, body) {
     if (resp) {
       if (resp.body) {
-        debugger
         var documents = resp.body.documents
         res.render('find_similar_results', {
           calls: documents
@@ -178,7 +160,6 @@ function obtainAudio(CallSid) {
           if (data.recordings.length > 0) { //if there is a recording
             console.log(data.recordings)
             data.recordings.forEach(function(recording) {
-              debugger
               var recordingUrl = "https://api.twilio.com"+recording.uri.split(".")[0]+".mp3"
               Call.update({'CallSid': CallSid}, {
                 audio: true,
@@ -198,88 +179,65 @@ function obtainAudio(CallSid) {
 }
 
 app.get('/processCall', function(req, res) {
-  debugger
   var CallSid = req.query.CallSid
   var language = req.query.language
   var entityType = req.query.entity_type
   Call.findOne({ CallSid: CallSid }, function(error, doc1) {
     if (doc1.processing == false) {
-      //
       console.log("Processing call: " + CallSid)
       Call.update({'CallSid': CallSid}, {
         processing: true,
         language: language,
         entitiesSelected: entityType
       }, function(error1, numberAffected, rawResponse) {
-        //
         if (error1) {
           console.log(error1)
         } else {
-          Call.findOne({ CallSid: CallSid }, function (error2, doc){
-            // doc.name = 'jason borne'
-            // doc.visits.$inc()
-            // doc.save()
+          Call.findOne({ CallSid: CallSid }, function (error2, doc) {
             if (error2) {
-
+              console.log(error2)
             } else {
               var recordingUrl = doc.RecordingUrl
               var To = doc.To
               var From = doc.From
               var dateCreated = doc.dateCreated
-              //
               var data1 = {url: recordingUrl, language: language, interval: 0}
-              // debugger
               hodClient.call('recognizespeech', data1, true, function(err1, resp1, body1) {
                 var jobID = resp1.body.jobID
-                // debugger
                 getAsyncResult(jobID, function(body) {
-                  // debugger
                   if (body == 'failed') {
                     createError(CallSid)
                   } else {
-                    //continue
-                    // var text = body.actions[0].result.document[0].content
                     var textSnippets = body.actions[0].result.document
-                    //
                     processText(textSnippets, function(textObj) {
-                      // debugger
                       var text = textObj.text
                       var confidenceAggregate = textObj.aggregate
-                      //
                       console.log("Text: " + text)
                       if (text == "") {
                         createError(CallSid)
                       } else {
-                        // HOD stuff
+                        // Analyze using HOD
                         var sentimentAnalysisLanguage = languageObj[language]['sentiment-analysis']
                         var data2 = {text: text, language: sentimentAnalysisLanguage}
                         hodClient.call('analyzesentiment', data2, function(err2, resp2, body2) {
-                          // debugger
                           console.log("Analyzed sentiment")
                           var sentimentResponse = body2
                           var aggregateSentiment = resp2.body.aggregate.sentiment
                           var aggregateScore = resp2.body.aggregate.score
                           hodClient.call('extractconcepts', data2, function(err3, resp3, body3) {
-                            // debugger
                             console.log("Extracted concepts")
                             var conceptsResponse = body3
                             var data3 = {text: text, entity_type: entityType}
                             hodClient.call('extractentities', data3, function(err4, resp4, body4) {
                               console.log("Extracted entities")
                               var entityResponse = body4
-                              //
-                              //
                               var json = {
                                 document: [
                                   {
-                                    // title: counter.toString(),
-                                    // body: body
                                     content: text,
                                     CallSid: CallSid,
-                                    // sentiments: sentimentResponse,
                                     aggregate_sentiment: aggregateSentiment,
                                     aggregate_score: aggregateScore,
-                                    // concepts: conceptsResponse,
                                     RecordingUrl: recordingUrl,
                                     TranscriptionText: text,
                                     confidence: confidenceAggregate,
@@ -294,10 +252,8 @@ app.get('/processCall', function(req, res) {
                                 index: process.env.HOD_INDEX_NAME,
                                 json: JSON.stringify(json)
                               }
-                              // debugger
                               hodClient.call('addtotextindex', data4, function(err5, resp5, body5) {
-                                // mongo
-                                // debugger
+                                // Mongo
                                 var indexReference = resp5.body.references[0].reference
                                 Call.update({'CallSid': CallSid}, {
                                   text: text,
@@ -313,27 +269,19 @@ app.get('/processCall', function(req, res) {
                                 }, function(err, numberAffected, rawResponse) {
                                   console.log("Processed")
                                 })
-                                //
                               })
-                              //
                             })
                           })
                         })
                       }
-                      // HOD stuff
-                      //
                     })
                   }
                 })
               })
-              //
-
             }
           })
         }
       })
-
-      //
     } else {
       console.log("Call already processing! " + CallSid)
       res.redirect('/')
@@ -343,7 +291,6 @@ app.get('/processCall', function(req, res) {
 
 function getAsyncResult(jobID, callback) {
   hodClient.getJobStatus(jobID, function(err, resp, body) {
-    // debugger
     if (resp.body['status'] != 'finished') {
       if (resp.body.actions[0].status == 'failed') {
         callback('failed')
@@ -362,7 +309,6 @@ function processText(textSnippetsArray, callback) {
   var counter = 1
   var text = ''
   async.each(textSnippetsArray, function(textInformation, c) {
-    // debugger
     var confidence = textInformation['confidence']
     text += textInformation['content']
     newAverage = oldAverage * (counter-1)/counter + confidence/counter;   // New average = old average * (n-1)/n + new value /n
@@ -386,11 +332,9 @@ function createError(callSid) {
 }
 
 app.post('/makeCall', function(req, res) {
-  // var query = req.query
   var phonenumber = req.body.phonenumber
-  // var phoneNumber = query.phoneNumber
   twilioClient.calls.create({
-      url: twiMLUrl,
+      url: process.env.TWI_ML_URL,
       to: phonenumber,
       from: process.env.TWILIO_PHONE_NUMBER
   }, function(err, call) {
